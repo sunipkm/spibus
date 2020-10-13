9067a655faffd6130b9fd8bc2aabddc76fdf2ef4
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2020-10-12 23:58:32 -0400

    SPACE HAUC SPI Driver (alpha)

---
+++ spibus.c
@@ -0,0 +1,187 @@
+#include <spibus.h>
+#include <string.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <signal.h>
+#include <gpio/gpiodev_extern.h>
+
+int spibus_init(spibus *dev)
+{
+    int file;
+    __u8 mode, lsb, bits;
+    __u32 speed;
+
+    mode = dev->mode;
+    lsb = dev->lsb;
+    bits = 0;
+
+    if (dev->bits == 0) // in case bits is zero, means bits was not set by spibus device init
+    {
+        dev->bits = 8;
+    }
+
+    if (speed == 0) // in case max speed was not stipulated
+    {
+        speed = 1000000; // 1 MHz
+        dev->speed = speed;
+    }
+
+    if (dev->cs_internal == CS_EXTERNAL) // GPIO as chip select
+    {
+        gpioWrite(dev->cs_gpio, GPIO_HIGH);
+    }
+
+    // open SPI bus
+    char spibusname[256];
+    if (snprintf(spibusname, 256, "/dev/spidev%d.%d", dev->bus, dev->cs) < 0)
+    {
+        printf("%s: Error in creating device bus name\n");
+        return -1;
+    }
+
+    if ((file = open(spibusname, O_RDWR)) < 0)
+    {
+        perror("SPIBUS: Opening bus");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_WR_MODE, &mode) < 0)
+    {
+        perror("SPIBUS: SPI write mode");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_RD_MODE, &mode) < 0)
+    {
+        perror("SPIBUS: SPI read mode");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_WR_LSB_FIRST, &lsb) < 0)
+    {
+        perror("SPIBUS: SPI write lsb mode");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_RD_LSB_FIRST, &lsb) < 0)
+    {
+        perror("SPIBUS: SPI read lsb mode");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_WR_BITS_PER_WORD, &bits) < 0)
+    {
+        perror("SPIBUS: SPI write bits per word");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0)
+    {
+        perror("SPIBUS: SPI read bits per word");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0)
+    {
+        perror("SPIBUS: SPI write speed hz");
+        return -1;
+    }
+
+    if (ioctl(file, SPI_IOC_RD_MAX_SPEED_HZ, &speed) < 0)
+    {
+        perror("SPIBUS: SPI read speed hz");
+        return -1;
+    }
+
+    dev->fd = file; // Update the file descriptor
+
+    dev->xfer[0].cs_change = 0;             // Keep CS activated for the whole word
+    dev->xfer[0].delay_usecs = 0;           // delay in microseconds
+    dev->xfer[0].speed_hz = speed;          // speed of communication
+    dev->xfer[0].bits_per_word = dev->bits; // bits per word
+
+    return 1;
+}
+
+int spibus_xfer(spibus *dev, void *data, ssize_t len)
+{
+    int status = 0;
+#ifdef SPIDEBUG
+    unsigned char *tmp = data;
+    fprintf(stderr, "%s: ", __func__);
+    for (unsigned i = 0; i < len; i++)
+        fprintf(stderr, "%02X ", tmp[i]);
+    fprintf(stderr, "\n\n");
+#endif
+    dev->xfer[0].tx_buf = (unsigned long)data;
+    dev->xfer[0].len = len;
+    if (dev->cs_internal == CS_EXTERNAL) // chip select is not internal
+    {
+        gpioWrite(dev->cs_gpio, GPIO_LOW); // active low transfer
+    }
+    status = ioctl(dev->fd, SPI_IOC_MESSAGE(1), dev->xfer);
+    if (dev->cs_internal == CS_EXTERNAL)
+    {
+        gpioWrite(dev->cs_gpio, GPIO_HIGH); // high after transfer
+    }
+    if (status < 0)
+    {
+        perror("SPIBUS: SPI transfer");
+        return -1;
+    }
+    usleep(dev->sleeplen);
+    return status;
+}
+
+int spibus_xfer_full(spibus *dev, void *in, ssize_t ilen, void *out, ssize_t olen)
+{
+    int status = 0;
+#ifdef SPIDEBUG
+    unsigned char *tmp = in;
+    fprintf(stderr, "%s: Out: ", __func__);
+    for (unsigned i = 0; i < ilen; i++)
+        fprintf(stderr, "%02X ", tmp[i]);
+    fprintf(stderr, "\n\n");
+#endif
+    dev->xfer[0].tx_buf = (unsigned long)in;
+    dev->xfer[0].rx_buf = (unsigned long)out;
+    dev->xfer[0].len = ilen < olen ? ilen : olen; // whichever is shorter to avoid access violation
+    if (dev->cs_internal == CS_EXTERNAL)          // chip select is not internal
+    {
+        gpioWrite(dev->cs_gpio, GPIO_LOW); // active low transfer
+    }
+    status = ioctl(dev->fd, SPI_IOC_MESSAGE(1), dev->xfer);
+    if (dev->cs_internal == CS_EXTERNAL)
+    {
+        gpioWrite(dev->cs_gpio, GPIO_HIGH); // high after transfer
+    }
+    if (status < 0)
+    {
+        perror("SPIBUS: SPI transfer");
+        return -1;
+    }
+#ifdef SPIDEBUG
+    tmp = out;
+    fprintf(stderr, "%s: In: ", __func__);
+    for (unsigned i = 0; i < olen; i++)
+        fprintf(stderr, "%02X ", tmp[i]);
+    fprintf(stderr, "\n\n");
+#endif
+    usleep(dev->sleeplen);
+    return status;
+}
+
+void spibus_destroy(spibus *dev)
+{
+    close(dev->fd);
+    free(dev);
+}
---
+++ spibus.h
@@ -0,0 +1,95 @@
+/**
+ * @file spibus.h
+ * @author Sunip K. Mukherjee (sunipkmukherjee@gmail.com)
+ * @brief SPI Bus driver data structures and function prototypes for SPACE HAUC
+ * @version 0.1
+ * @date 2020-10-12
+ * 
+ * @copyright Copyright (c) 2020
+ */
+#ifndef SPI_BUS_H
+#define SPI_BUS_H
+
+#include <macros.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+#define CS_INTERNAL 1
+#define CS_EXTERNAL 0
+
+typedef struct __attribute__((packed))
+{
+    int fd;                          /// SPI File Descriptor
+    struct spi_ioc_transfer xfer[1]; /// SPI Transfer IO buffer
+    uint8_t bus;                     /// SPI bus device (X in /dev/spidevX.Y)
+    uint8_t cs;                      /// SPI bus CS (Y in /dev/spidevX.Y)
+    uint8_t mode;                    /// SPI Mode (SPI_MODE0 etc)
+    uint8_t lsb;                     /// LSB first, 0 if MSB first
+    uint8_t bits;                    /// Bits per word, usually 8
+    uint32_t speed;                  /// Max speed of device
+    uint8_t cs_internal;             /// If this var is set, SPI bus has its own CS, if unset, GPIO needs to be evoked
+    int cs_gpio;                     /// GPIO chipselect
+    unsigned long sleeplen;          /// Sleep length between two transfers in microseconds
+} spibus;
+
+/**
+ * @brief Initializes SPI bus with pre-configured settings. Settings are preconfigured
+ * by device initializer whose member is the spibus struct that is passed as input.
+ * 
+ * @param dev spibus struct containing SPI Bus number to open, mode, speed, lsbfirst etc
+ * 
+ * @return 1 on success, -1 on failure.
+ */
+int spibus_init(spibus *dev);
+/**
+ * @brief Transfer data over SPI bus described by dev.
+ * 
+ * @param dev spibus struct describing the SPI bus
+ * @param data Ideally a buffer of bytes. The transfer function will not flip
+ * little endian data to meet MSBFirst data transfers, though the spibus library
+ * provides a function for the inversion.
+ * @param len Length of the supplied buffer (in bytes)
+ * 
+ * @return status of the ioctl call for the transfer
+ */
+int spibus_xfer(spibus *dev, void *data, ssize_t len);
+/**
+ * @brief Transfer data over SPI bus described by dev.
+ * 
+ * @param dev spibus struct describing the SPI bus
+ * @param in Ideally a buffer of bytes. The transfer function will not flip
+ * little endian data to meet MSBFirst data transfers, though the spibus library
+ * provides a function for the inversion.
+ * @param ilen Length of the supplied buffer (in bytes)
+ * @param out Pointer to output data buffer (bytes)
+ * @param olen Length of expected output (bytes)
+ * 
+ * @return status of the ioctl call for the transfer
+ */
+int spibus_xfer_full(spibus *dev, void *in, ssize_t ilen, void *out, ssize_t olen);
+/**
+ * @brief Destroy the SPI bus. For errors, look at close() syscall.
+ */
+void spibus_destroy(spibus *dev);
+/**
+ * @brief Invert array for MSB first transfer of multibyte data. Memory
+ * management is entirely upon the caller.
+ * 
+ * @param dest Destination pointer
+ * @param src Source pointer
+ * @param len Length of source and destination buffers.
+ */
+inline void spibus_invert(void *dest, void *src, ssize_t len)
+{
+    unsigned int last = len - 1;
+    unsigned char *tmpsrc, *tmpdest;
+    for (unsigned int i = 0; i < len; i++)
+    {
+        tmpdest[i] = tmpsrc[last - i];
+    }
+    return;
+}
+
+#endif // SPI_BUS_H
